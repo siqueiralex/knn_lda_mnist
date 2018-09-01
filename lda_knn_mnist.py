86332b1c5e9bb708ec7bc4e3a5840385996846a5
Alex Siqueira Lacerda <Lequizi@gmail.com>
2018-09-01 13:41:28 -0300

    changing k's and adding hit rate

--- lda_knn_mnist.py
+++ lda_knn_mnist.py
@@ -10,8 +10,19 @@ def print_report(predictions, targets):
     confusion_matrix = np.zeros((10, 10), dtype=np.int)
     for i in range(len(predictions)):
         confusion_matrix[targets[i]][predictions[i]]+=1
-    print(confusion_matrix)    
-    print(classification_report(targets, predictions)) 
+    
+    hit_rate = np.zeros(10)
+
+    for i in range(0,10):
+    	sum = 0
+    	for j in range(0,10):
+    		sum += confusion_matrix[i][j]
+    	hit_rate[i] = confusion_matrix[i][i]/sum	
+
+
+    print(confusion_matrix)
+    print(hit_rate)    
+    #print(classification_report(targets, predictions)) 
 
 path = os.path.join("datasets", "mnist")
 mndata = MNIST(path)
@@ -19,28 +30,28 @@ images, labels = mndata.load_training()
 test_images, test_labels = mndata.load_testing()
 
 #sample tests
-test_images = test_images[:1000]
-test_labels = test_labels.tolist()[:1000]
+#test_images = test_images[:1000]
+#test_labels = test_labels.tolist()[:1000]
 
-k = 5
+k = 3
 knn = KNeighborsClassifier(n_neighbors=k, n_jobs=-1)
 knn.fit(images, labels.tolist())
 predictions = knn.predict(test_images)
 
-print("KNN k=10")
+print("KNN k=3")
 print_report(predictions, test_labels)
 print()
 
-knn.n_neighbors=10
+knn.n_neighbors=5
 predictions = knn.predict(test_images)
-print("KNN k=100")
+print("KNN k=5")
 print_report(predictions, test_labels)    
 print()
 
 
-knn.n_neighbors=244
+knn.n_neighbors=10
 predictions = knn.predict(test_images)
-print("KNN k=244 -> sqrt(60k)")
+print("KNN k=10")
 print_report(predictions, test_labels)    
 print()
 
